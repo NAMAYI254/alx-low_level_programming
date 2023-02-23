197c3cb6af5d6b5f2f695913780223eefea6b26d
NAMAYI <franklynenm@gmail.com>
2023-02-23 15:17:04 -0800

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,8 +9,8 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-			_putchar('\n');
-		}
+		_putchar('\n');
+	}
 	else
 	{
 		int i, j;
@@ -20,7 +20,7 @@ void print_square(int size)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar('#');			}
+			_putchar('#');			}
 			_putchar('\n');
 		}
 	}
